239fbd019b120f9730464b7916d4e80ec43b2f17
dgzborromeo <dgzborromeo88@gmail.com>
2026-03-23 15:40:20 +0800

    API/ScheduleController

--- routes/api.js
+++ routes/api.js
@@ -77,6 +77,7 @@ router.delete('/schedule/:id', configController.deleteSchedule);
 
 router.get('/getSchedules', scheduleController.getAllSched);
 router.get('/getSchedule/:id', scheduleController.getSchedById);
+router.post('/check-schedule-conflict', scheduleController.checkConflict);
 router.post('/add-schedule', uploadSchedule.single('attachment_file'), scheduleController.createSched);
 router.post('/update-schedule/:id', uploadSchedule.single('attachment_file'), scheduleController.updateSched);
 router.delete('/delete-schedule/:id', scheduleController.deleteSched);
